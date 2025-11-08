c57de2e347c6dcb725671f3e8e641b1230a5040b
Edgar <zyneticartz@gmail.com>
2025-11-08 18:42:08 +0100

    Fix dotenv path for Railway

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: '../.env' })
+require('dotenv').config()
 const { Client, GatewayIntentBits, PermissionFlagsBits, ChannelType, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js')
 const axios = require('axios')
 
